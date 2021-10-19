345f7ef61cc48bc07af67d8714067aef44087d33
paval_miatlitski <paval.escoba@gmail.com>
2021-10-20 01:24:54 +0300

    add: html to pdf add and test

--- scripts/main.js
+++ scripts/main.js
@@ -1,11 +1,27 @@
 window.addEventListener('load', () => {
-  function genPDF() {
-    var doc = new jsPDF('l');
+  // function genPDF() {
+  //   var doc = new jsPDF('l');
 
-    doc.lines([[2, 2], [-2, 2], [1, 1, 2, 2, 3, 3], [2, 1]], 212, 110, 10);
-    doc.save('example.pdf');
+  //   doc.lines([[2, 2], [-2, 2], [1, 1, 2, 2, 3, 3], [2, 1]], 212, 110, 10);
+  //   doc.save('example.pdf');
+  // };
+
+  // const downloadBtn = document.querySelector('#download');
+  // downloadBtn.addEventListener('click', genPDF);
+
+  const section = document.querySelector('#section');
+  const downloadBtn = document.querySelector('#download');
+  var opt = {
+    margin: 1,
+    filename: 'myfile.pdf',
+    image: { type: 'jpeg' },
+    jsPDF: { unit: 'in', format: 'letter', orientation: 'portrait' }
   };
 
-  const downloadBtn = document.querySelector('#download');
-  downloadBtn.addEventListener('click', genPDF);
+  const downloadHandler = () => {
+    html2pdf().from(section).save();
+  }
+
+  downloadBtn.addEventListener('click', downloadHandler);
+
 });
