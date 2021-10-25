375606ccb5a11ba2f78940bffe932c8e68c37f1d
paval_miatlitski <paval.escoba@gmail.com>
2021-10-26 02:02:38 +0300

    add: ptototyping. one line, with nib blocks for foundational hand

--- scripts/main.js
+++ scripts/main.js
@@ -9,19 +9,39 @@ window.addEventListener('load', () => {
   // const downloadBtn = document.querySelector('#download');
   // downloadBtn.addEventListener('click', genPDF);
 
-  const section = document.querySelector('#section');
+  const section = document.querySelector('#sheet');
   const downloadBtn = document.querySelector('#download');
-  var opt = {
+  const styleSelect = document.querySelector('#writting');
+  const nibSelect = document.querySelector('#nib');
+  const opt = {
     margin: 1,
     filename: 'myfile.pdf',
     image: { type: 'jpeg' },
-    jsPDF: { unit: 'in', format: 'letter', orientation: 'portrait' }
+    jsPDF: { unit: 'mm', format: 'letter', orientation: 'portrait' }
   };
 
   const downloadHandler = () => {
     html2pdf().from(section).save();
+  };
+
+  const canvasProps = {
+    foundational: { base: 5, asceder: 3, descender: 3, interline: 1, nibAngle: 30, slope: 0 },
+    fraktur: { base: 5.5, asceder: 2.5, descender: 2.5, interline: 1, nibAngle: 45, slope: 0 }
+  }
+  const PX_TO_MM_RATIO = 3.78;
+
+
+
+  const drawGuidelines = (nibWidth, style) => {
+
+  }
+
+  const selectHandler = (e) => {
+    console.log(nibSelect.value);
   }
 
   downloadBtn.addEventListener('click', downloadHandler);
+  nibSelect.addEventListener('change', selectHandler);
+  styleSelect.addEventListener('click', selectHandler);
 
 });
